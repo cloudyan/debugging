c6fda9d4545dd6acd9e0e984c51c8df2f14b70fd
cloudyan <1395093509@qq.com>
2020-09-10 23:07:30 +0800

    feat: update

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
 const hello = name => `hello ${name}`
 
 console.log(hello('jack'))
+
+
+const aa = /((\d){0,32}(\.)?(\d){0,32}){32,32}/g
+
+function t(n) {
+  console.log(aa.test(n))
+}
+t(12345678901234567890123456789012)
+t(12345678901234567890.12345678901)
+t(12345678901234567890.1234567890)
